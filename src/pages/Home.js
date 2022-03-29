74bc3c2bf3083671f9fa511348c5f30c4ff402f1
deonvisser44 <deonvisser44@gmail.com>
2022-03-29 14:17:34 +0700

    Added loading animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,35 +4,30 @@ import BlogCard from "../components/BlogCard";
 import PersonalCard from "../components/PersonalCard";
 import Toast from "../components/Toast";
 import { db } from "../firebase";
-import {
-  query,
-  orderBy,
-  collection,
-  onSnapshot,
-} from "firebase/firestore";
-
+import { query, orderBy, collection, onSnapshot } from "firebase/firestore";
+import loading from '../assets/loading.png'
 
 function Home(props) {
-  
- const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
- useEffect(() => {
-  const q = query(collection(db, "posts"), orderBy("date", "desc"));
-  const unsub = onSnapshot(q, (querySnapshot) => {
-    let postsArray = [];
-    querySnapshot.forEach((doc) => {
-      postsArray.push({ ...doc.data(), id: doc.id });
+  useEffect(() => {
+    const q = query(collection(db, "posts"), orderBy("date", "desc"));
+    const unsub = onSnapshot(q, (querySnapshot) => {
+      let postsArray = [];
+      querySnapshot.forEach((doc) => {
+        postsArray.push({ ...doc.data(), id: doc.id });
+      });
+      setPosts(postsArray);
+      setIsLoading(false)
     });
-    setPosts(postsArray);
-  });
-  return () => unsub();
-}, []);
-
-
+    return () => unsub();
+  }, []);
 
   return (
     <div className={classes.home}>
       <PersonalCard />
+      {isLoading && <img className={classes.loading} src={loading}/>}
       <ul className={classes.list}>
         {posts.map((post) => (
           <BlogCard
@@ -45,7 +40,7 @@ function Home(props) {
           />
         ))}
       </ul>
-       {props.showToast && <Toast/>}
+      {props.showToast && <Toast />}
     </div>
   );
 }
