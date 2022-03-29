9a3de2aad2c766873d75ae4bdd9d25edcd514477
deonvisser44 <deonvisser44@gmail.com>
2022-03-29 08:59:13 +0700

    Changed date font-size

--- src/components/PersonalCard.js
+++ src/components/PersonalCard.js
@@ -9,7 +9,7 @@ function PersonalCard() {
         <img className={classes.image} src={image} alt="self portrait" />
       </div>
       <p className={classes.body}>
-        Hi there! I built this blog along with a CMS to post and manage its
+        Hi! I'm Deon Visser, web developer CS student. I built this blog along with a CMS to post and manage its
         content. I will be using it to post about my learning progress as a web
         developer. Thanks for stopping by.
       </p>
